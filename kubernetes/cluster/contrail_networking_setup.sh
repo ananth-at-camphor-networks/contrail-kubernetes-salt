074df01c64f99756ecb3e75d9ad718f026c3e600
Ananth Suryanarayana <anantharamu@gmail.com>
2015-08-28 11:28:15 -0700

    update install script

--- kubernetes/cluster/contrail_networking_setup.sh
+++ kubernetes/cluster/contrail_networking_setup.sh
@@ -1,53 +1,97 @@
 #!/bin/bash
 
-set -e
+function retry() {
+    set +e
+
+    CMD=$1
+    echo $CMD
+    shift
+
+    if [ -z $RETRY ]; then
+        RETRY=10
+    fi
+    if [ -z $WAIT ]; then
+        WAIT=10
+    fi
+
+    COUNTER=0
+    while [  $COUNTER -lt $RETRY ]; do
+        $CMD $*
+        if [ "$?" = "0" ]; then
+            return
+        fi
+        let COUNTER=COUNTER+1
+        echo Try again $COUNTER/$RETRY
+        sleep $WAIT
+    done
+
+    set -e
+
+    echo "Error EXIT: $CMD $* Failed with exit code $?"
+    exit -1
+}
 
 function master() {
     ssh -oStrictHostKeyChecking=no -i "${SSH_KEY}" "${SSH_USER}@${KUBE_MASTER_IP}" sudo "$*"
 }
 
 function verify_contrail_listen_services() {
-    master netstat -anp | \grep LISTEN | \grep -w 5672 # RabbitMQ
-    master netstat -anp | \grep LISTEN | \grep -w 2181 # ZooKeeper
-    master netstat -anp | \grep LISTEN | \grep -w 9160 # Cassandra
-    master netstat -anp | \grep LISTEN | \grep -w 5269 # XMPP Server
-    master netstat -anp | \grep LISTEN | \grep -w 8083 # Control-Node Introspect
-    master netstat -anp | \grep LISTEN | \grep -w 8443 # IFMAP
-    master netstat -anp | \grep LISTEN | \grep -w 8082 # API-Server
-    master netstat -anp | \grep LISTEN | \grep -w 8087 # Schema
-    master netstat -anp | \grep LISTEN | \grep -w 5998 # discovery
-    master netstat -anp | \grep LISTEN | \grep -w 8086 # Collector
-    master netstat -anp | \grep LISTEN | \grep -w 8081 # OpServer
-    master netstat -anp | \grep LISTEN | \grep -w 8091 # query-engine
-    master netstat -anp | \grep LISTEN | \grep -w 6379 # redis
-    master netstat -anp | \grep LISTEN | \grep -w 8143 # WebUI
-    master netstat -anp | \grep LISTEN | \grep -w 8070 # WebUI
-    master netstat -anp | \grep LISTEN | \grep -w 3000 # WebUI
+    RETRY=20
+    WAIT=3
+    retry master 'netstat -anp | grep LISTEN | grep -w 5672' # RabbitMQ
+    retry master 'netstat -anp | grep LISTEN | grep -w 2181' # ZooKeeper
+    retry master 'netstat -anp | grep LISTEN | grep -w 9160' # Cassandra
+    retry master 'netstat -anp | grep LISTEN | grep -w 5269' # XMPP Server
+    retry master 'netstat -anp | grep LISTEN | grep -w 8083' # Control-Node Introspect
+    retry master 'netstat -anp | grep LISTEN | grep -w 8443' # IFMAP
+    retry master 'netstat -anp | grep LISTEN | grep -w 8082' # API-Server
+    retry master 'netstat -anp | grep LISTEN | grep -w 8087' # Schema
+    retry master 'netstat -anp | grep LISTEN | grep -w 5998' # discovery
+    retry master 'netstat -anp | grep LISTEN | grep -w 8086' # Collector
+    retry master 'netstat -anp | grep LISTEN | grep -w 8081' # OpServer
+    retry master 'netstat -anp | grep LISTEN | grep -w 8091' # query-engine
+    retry master 'netstat -anp | grep LISTEN | grep -w 6379' # redis
+    retry master 'netstat -anp | grep LISTEN | grep -w 8143' # WebUI
+    retry master 'netstat -anp | grep LISTEN | grep -w 8070' # WebUI
+    retry master 'netstat -anp | grep LISTEN | grep -w 3000' # WebUI
 }
 
 function provision_bgp() {
-    master docker ps |\grep contrail-api |\grep -v pause | awk '{print "docker exec " $1 " curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_control.py -o /tmp/provision_control.py"}' | sh
-    master docker ps |\grep contrail-api |\grep -v pause | awk '{print "docker exec " $1 " curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_bgp.py -o /tmp/provision_bgp.py"}' | sh
-    master docker ps |\grep contrail-api |\grep -v pause | awk '{print "docker exec " $1 " python /tmp/provision_control.py  --router_asn 64512 --host_name `hostname` --host_ip `hostname --ip-address` --oper add --api_server_ip `hostname --ip-address` --api_server_port 8082"}' | sh
+    cmd='docker ps | grep contrail-api | grep -v pause | awk "{print \"docker exec \" \$1 \" curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_control.py -o /tmp/provision_control.py\"}" | sudo sh'
+    master $cmd
+    cmd='docker ps | grep contrail-api | grep -v pause | awk "{print \"docker exec \" \$1 \" curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_bgp.py -o /tmp/provision_bgp.py\"}" | sudo sh'
+    master $cmd
+    cmd='docker ps | grep contrail-api | grep -v pause | awk "{print \"docker exec \" \$1 \" python /tmp/provision_control.py  --router_asn 64512 --host_name `hostname` --host_ip `hostname --ip-address` --oper add --api_server_ip `hostname --ip-address` --api_server_port 8082\"}" | sudo sh'
+    master $cmd
 }
 
 function provision_linklocal() {
-    master docker ps |\grep contrail-api |\grep -v pause | awk '{print "docker exec " $1 " curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_linklocal.py -o /tmp/provision_linklocal.py"}' | sh
-    master docker ps |\grep contrail-api |\grep -v pause | awk '{print "docker exec " $1 " python /tmp/provision_linklocal.py --api_server_ip `hostname --ip-address` --api_server_port 8082 --linklocal_service_name kubernetes --linklocal_service_ip 10.0.0.1 --linklocal_service_port 8080 --ipfabric_service_ip `hostname --ip-` --ipfabric_service_port 8080 --oper add"}' | sh
+    cmd='docker ps | grep contrail-api | grep -v pause | awk "{print \"docker exec \" \$1 \" curl -s https://raw.githubusercontent.com/Juniper/contrail-controller/R2.20/src/config/utils/provision_linklocal.py -o /tmp/provision_linklocal.py\"}" | sudo sh'
+    master $cmd
+    cmd='docker ps | grep contrail-api | grep -v pause | awk "{print \"docker exec \" \$1 \" python /tmp/provision_linklocal.py --api_server_ip `hostname --ip-address` --api_server_port 8082 --linklocal_service_name kubernetes --linklocal_service_ip 10.0.0.1 --linklocal_service_port 8080 --ipfabric_service_ip `hostname --ip-` --ipfabric_service_port 8080 --oper add\"}" | sudo sh'
+    master $cmd
 }
 
 function setup_kube_dns_endpoints() {
-    master kubectl --namespace=kube-system create -f /etc/kubernetes/addons/kube-ui/kube-ui-endpoint.yaml
-    master kubectl --namespace=kube-system create -f /etc/kubernetes/addons/kube-ui/kube-ui-svc-address.yaml
+    master kubectl --namespace=kube-system create -f /etc/kubernetes/addons/kube-ui/kube-ui-endpoint.yaml || true
+    master kubectl --namespace=kube-system create -f /etc/kubernetes/addons/kube-ui/kube-ui-svc-address.yaml || true
 }
 
-function pull_docker_images() {
-    cmd='grep source: /srv/salt/contrail-*/* | awk "{print $4}" | xargs -n 1 wget -qO - | grep \"image\": | cut -d "\"" -f 4 | xargs -n1 sudo docker pull'
+function setup_contrail_manifest_files() {
+    cmd='wget -qO - https://raw.githubusercontent.com/rombie/contrail-kubernetes/fedora_ubuntu_demo/cluster/manifests.hash | awk "{print \"https://raw.githubusercontent.com/rombie/contrail-kubernetes/fedora_ubuntu_demo/cluster/\"\$1}" | xargs -n1 sudo wget -q --directory-prefix=/etc/contrail/manifests --continue'
     master $cmd
-    master grep source: /srv/salt/contrail-*/* | awk '{print $4}' | xargs -n1 wget -q --directory-prefix=/etc/kubernetes/manifests
+    cmd='grep \"image\": /etc/contrail/manifests/* | cut -d "\"" -f 4 | sort -u | xargs -n1 sudo docker pull'
+    master $cmd
+    cmd='mv /etc/contrail/manifests/* /etc/kubernetes/manifests/'
+    master $cmd
+
+#   cmd='grep source: /srv/salt/contrail-*/* | awk "{print $4}" | xargs -n 1 wget -qO - | grep \"image\": | cut -d "\"" -f 4 | xargs -n1 sudo docker pull'
+#   master $cmd
+#   master grep source: /srv/salt/contrail-*/* | awk '{print $4}' | xargs -n1 wget -q --directory-prefix=/etc/kubernetes/manifests
 }
 
 function setup_contrail_networking() {
+    SAVED_OPTIONS=$(set +o)
     set -x
 
     SSH_KEY=$1
@@ -55,7 +99,7 @@ function setup_contrail_networking() {
     KUBE_MASTER_IP=$3
 
     # Pull all contrail images and copy the manifest files
-    pull_docker_images
+    setup_contrail_manifest_files
 
     # Wait for contrail-control to be ready.
     verify_contrail_listen_services
@@ -70,5 +114,5 @@ function setup_contrail_networking() {
     setup_kube_dns_endpoints
 
     # setup_minions
-    exit
+    eval "$SAVED_OPTIONS"
 }
